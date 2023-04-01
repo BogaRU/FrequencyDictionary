0e2c8a344827156233af8c6de2341de5ede477af
Богдан Ведерников <botom123456@mail.ru>
2023-04-01 20:33:51 +0300

    More user friendly

--- Program.cs
+++ Program.cs
@@ -10,15 +10,46 @@ namespace TextAnalysis
 {
     internal static class Program
     {
-        static string TextFile = @"./HarryPotterText.txt";
+        static string filePath;
+        static int wordsNumber;
         public static void Main(string[] args)
         {
-            var text = File.ReadAllText(TextFile);
+            Initialize();
+
+            Console.WriteLine("Пожалуйста, подождите...");
+            var text = File.ReadAllText(filePath);
             var sentences = SentencesParserTask.ParseSentences(text);
             var frequency = FrequencyAnalysisTask.GetMostFrequentNextWords(sentences);
-            Console.WriteLine("Введи любое слово/предложение:");
+            Console.Clear();
+
+            Console.WriteLine("Введите любое слово/предложение:\n");
             while (true)
-                Console.WriteLine("Частотный словарь говорит:\n" + TextGeneratorTask.ContinuePhrase(frequency, Console.ReadLine(), 8) + "\nВведи любое слово/предложение:");
+                Console.WriteLine("\nЧастотный словарь говорит:\n\n" + TextGeneratorTask.ContinuePhrase(frequency, Console.ReadLine(), wordsNumber) + "\n\nВведите любое слово/предложение:\n");
+        }
+
+        static void Initialize()
+        {
+            Console.WriteLine("Введите источник текста:");
+            Console.WriteLine("1. TXT файл\r\n2. JSON файл\n");
+
+            while (true)
+            {
+                string tmp = Console.ReadLine();
+                if (tmp == "1" || tmp == "2")
+                {
+                    Console.WriteLine("\nВведите путь к файлу:\n");
+                    filePath = Console.ReadLine();
+                    if (tmp == "2")
+                    {
+                        Console.WriteLine("\nВведите Id диалога:\n");
+                        parseNewFromTelegram(Convert.ToInt32(Console.ReadLine()));
+                    }
+                    Console.WriteLine("\nВведите количество выводимых слов с наибольшей частотностью:\n");
+                    wordsNumber = Convert.ToInt32(Console.ReadLine());
+                    Console.Clear();
+                    break;
+                }
+            }
         }
 
         public static bool isCorrect(string text)
@@ -33,13 +64,17 @@ namespace TextAnalysis
         }
 
         // Метод парсит файл result.json, полученный через экспорт диалогов в телеграме, складывает все сообщения в txt-файл, который затем используется в частотном словаре.
-        public static void parseNewFromTelegram(int N, string name)
+        public static void parseNewFromTelegram(int N)
         {
             // 0 - Gym, 5 - Bin, 3 - Jen, 14 - Svt
-            string filePath = "parsedDialog" + name + ".txt";
-            if (File.Exists(filePath)) return;
             string jsonText = File.ReadAllText("result.json");
             Telegram telegram = JsonConvert.DeserializeObject<Telegram>(jsonText);
+            string filePath = "parsedDialog" + telegram.chats.list[N].name + ".txt";
+            if (File.Exists(filePath))
+            {
+                Program.filePath = filePath;
+                return;
+            }
             StringBuilder parsedText = new StringBuilder("");
             foreach (var mes in telegram.chats.list[N].messages)
             {
@@ -48,6 +83,7 @@ namespace TextAnalysis
                 if (isCorrect(tmp)) parsedText.Append(tmp + " ");
             }
             File.WriteAllText(filePath, parsedText.ToString());
+            Program.filePath = "./" + filePath;
         }
     }
 }
